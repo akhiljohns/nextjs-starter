e1988c9d33e34b45c55e087b3058bb7eaab26c55
akhiljohns <akhiljohns250@gmail.com>
2025-12-18 11:10:19 +0530

    config: Remove ESLint configuration from Next.js build.
    it was deprecated in nextjs 16

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Enforce ESLint during build - blocks build if there are errors
-  eslint: {
-    // Run ESLint on all directories during build
-    dirs: ['src'],
-    // Do NOT ignore ESLint errors during build (strict mode)
-    ignoreDuringBuilds: false,
-  },
-
   // Enforce TypeScript type checking during build
   typescript: {
     // Do NOT ignore TypeScript errors during build (strict mode)
